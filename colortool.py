0a58464eda9139de9f7eda722dc37dc72e50a9e7
Alexander Rodionov <tandav@tandav.me>
2023-10-14 21:30:06 +0400

    bump version v0.5.1 -> v0.5.2

--- colortool.py
+++ colortool.py
@@ -6,7 +6,7 @@ import random
 import string
 import typing as tp
 
-__version__ = '0.5.1'
+__version__ = '0.5.2'
 
 Float3 = tp.Tuple[float, float, float]
 Float4 = tp.Tuple[float, float, float, float]
